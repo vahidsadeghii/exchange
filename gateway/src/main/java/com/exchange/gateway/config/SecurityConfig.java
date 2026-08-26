78a6b856129db59c0b09e8e643360a1d46741a92
homa <rezaieehoma@gmail.com>
2026-08-26 23:53:40 +0200

    EX-118-Create-Kuber-File: Edit RequestHandlerService

--- gateway/src/main/java/com/exchange/gateway/config/SecurityConfig.java
+++ gateway/src/main/java/com/exchange/gateway/config/SecurityConfig.java
@@ -1,16 +1,19 @@
 package com.exchange.gateway.config;
 
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 
 @Configuration
-@EnableWebFluxSecurity
+@RequiredArgsConstructor
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
